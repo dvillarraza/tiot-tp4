9fd6907d4c46d96ccc96fd49e0fb8786e3d2245a
dvillarraza <dvillarraza@hotmail.com>
2021-06-21 11:21:21 -0300

    Segunda Entrega con correciones

--- test.js
+++ test.js
@@ -14,39 +14,28 @@ describe('WebDriverTest', () => {
 
             //Carga la pagina www.google.com
             await driver.get('https://www.google.com');
-            //Hace una pausa de 1 seg
-            await driver.sleep(1000);
+ 
             //Escriba en el input de busqueda la palabra "iot"
-            await driver.findElement(By.name('q')).sendKeys('iot',Key.ENTER);
-
+            await driver.findElement(By.name('q')).sendKeys('tissot',Key.ENTER);
+ 
             //Busca todos los elementos que pertenecen a la clase donde estan los links buscados
             //los links patrocinados pertencen a otra clase
-            let elements = await driver.findElements(By.xpath("//div[@class='tF2Cxc']/div[@class='yuRUbf']"));
-
-            let j = 0;
-            let posBuscada = 0;
-            let urlBuscada;// = "";
-            //Recorre la lista de elementos encontrados
-            for(let i= 0; i < elements.length; i++)
-            {   
-                //De cada elemento obtiene el texto dado que existen elementos que estan vacios
-                let text = await elements[i].getText();
-                if (text != ""){
-                    //Acumula solo los elementos que tienen texto
-                    j++;
-                    //El tercer link buscado  
-                    if (j == 3){
-                        //Alamacena la posicion en la lista del tercer link
-                        posBuscada = i;  
-                    }
-                }
-            }
-
+             let elements = await driver.findElements(By.xpath("//div[@class='tF2Cxc']/div[@class='yuRUbf']"));
+ 
             //Se controla que se haya encontrado el link buscado
-            expect(posBuscada).greaterThanOrEqual(0);         
+            expect(elements.length).greaterThanOrEqual(3);         
 
             //Hace click en el link y entra a la pagina buscada
-            await elements[posBuscada].click();
+            await elements[2].click();
+
+            //Hace una pausa de 2 segundos para abrir la pagina
+            await driver.sleep(2000);
+
+            //Obtengo el titulo de la nueva pagina
+            let title = await driver.getTitle(); 
+
+            //Se controla que el titulo contenga la palabra buscada como control de que se haya echo click
+            expect(title).to.contain("Tissot");
 
             //Hace una pausa de 2 segundos para ver el resultado
             await driver.sleep(2000);
